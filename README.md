73b100325d66b528d506ec000feadb0c145b0122
Alex Plex <atellmer@gmail.com>
2016-10-04 22:00:18 +0500

    stylus added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Remple
+
+https://github.com/atellmer/template-for-newbies.git
+
+```sh
+git clone https://github.com/atellmer/template-for-newbies.git templ
+cd templ
+npm install
+bower install
+npm start
+```
+### Enjoy!
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
 var connect = require('gulp-connect');
 
 
@@ -11,23 +14,39 @@ gulp.task('connect', function () {
 	});
 });
 
-//reload
-gulp.task('reload', function () {
-	return gulp.src([
-		'client/**/*.html',
-		'client/**/*.css',
-		'client/**/*.js']).pipe(connect.reload());	
+//stylus
+gulp.task('styl', function () {
+	return gulp.src(['client/**/*.styl'])
+		.pipe(concat('bundle.styl'))
+		.pipe(stylus({
+			use: [nib()],
+			compress: false
+		}))
+		.pipe(gulp.dest('client/dist/'))
+		.pipe(connect.reload());
+});
+
+//html
+gulp.task('html', function () {
+	return gulp.src(['client/**/*.html'])
+  .pipe(connect.reload());	
+});
+
+//js
+gulp.task('scripts', function () {
+	return gulp.src(['client/**/*.js'])
+  .pipe(connect.reload());	
 });
 
 //watch
 gulp.task('watch', function () {
-	gulp.watch([
-		'client/**/*.html',
-		'client/**/*.css',
-		'client/**/*.js'], ['reload']);
+	gulp.watch(['client/**/*.html'], ['html']);
+  gulp.watch(['client/**/*.js'], ['scripts']);
+  gulp.watch(['client/**/*.styl'],['styl']);
 });
 
 gulp.task('default', [
 	'connect',
+  'styl',
 	'watch'
 ]);
